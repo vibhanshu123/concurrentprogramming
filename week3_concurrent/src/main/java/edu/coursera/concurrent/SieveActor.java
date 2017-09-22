a4ed6f58871b30323c0b02b8b6a68da03286ed72
vibhanshu <vibhanshu@vibhanshus-MacBook-Air.local>
2017-09-22 08:47:13 +0530

    week 3

--- week3_concurrent/src/main/java/edu/coursera/concurrent/SieveActor.java
+++ week3_concurrent/src/main/java/edu/coursera/concurrent/SieveActor.java
@@ -1,6 +1,13 @@
 package edu.coursera.concurrent;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import edu.rice.pcdp.Actor;
+import edu.rice.pcdp.PCDP;
+
+
+
 
 /**
  * An actor-based implementation of the Sieve of Eratosthenes.
@@ -9,6 +16,9 @@ import edu.rice.pcdp.Actor;
  * countPrimes to determin the number of primes <= limit.
  */
 public final class SieveActor extends Sieve {
+	
+	
+	
     /**
      * {@inheritDoc}
      *
@@ -19,24 +29,103 @@ public final class SieveActor extends Sieve {
      */
     @Override
     public int countPrimes(final int limit) {
-        throw new UnsupportedOperationException();
+    	//final List<Integer> localPrimes = new ArrayList<Integer>();
+    	
+    		 final SieveActorActor sieveActor= new SieveActorActor(2);
+    		 PCDP.finish(()->{
+    	        for (int i = 3; i <= limit; i += 2) {
+    	            sieveActor.send(i);
+    	        }
+
+    	        
+    	 });
+    	 return sieveActor.getLocalPrimes();
     }
 
     /**
      * An actor class that helps implement the Sieve of Eratosthenes in
      * parallel.
      */
-    public static final class SieveActorActor extends Actor {
-        /**
+    public final class SieveActorActor extends Actor {
+    	
+    	private final List<Integer> localPrimes = new ArrayList<Integer>();
+    	private SieveActorActor nextActor;
+    	
+    	
+        public SieveActorActor(int localPrime) {
+			super();
+			this.localPrimes.add(localPrime);
+			this.nextActor = null;
+		}
+
+
+
+		public SieveActorActor getNextActor() {
+			return nextActor;
+		}
+		
+		
+		
+
+
+		public Integer getLocalPrimes() {
+			return localPrimes.size();
+		}
+
+
+
+		/**
          * Process a single message sent to this actor.
          *
          * TODO complete this method.
          *
          * @param msg Received message
          */
-        @Override
-        public void process(final Object msg) {
-            throw new UnsupportedOperationException();
+	    @Override
+        public void process(Object msg) {
+           final int candidate = (Integer) msg;
+           if(candidate<=0) {
+        	   if(nextActor!=null) {
+        		   nextActor.send(msg);
+        	   }
+        	   
+           }else {
+        	    boolean locallyPrime = isLocallyPrime(candidate);
+        	   if(locallyPrime) {
+        			   localPrimes.add(candidate);
+        			   //numLocalPrimes+=1;
+        		   }else if(nextActor==null) {
+        			   nextActor= new SieveActorActor(candidate);
+        			  // nextActor.start();
+        		   }else {
+        			   nextActor.send(msg);
+        		   }
+        	   }
+           }
+	    
+	    private boolean isLocallyPrime(int candidate) {
+			// TODO Auto-generated method stub
+			final boolean[] isPrime = {true};
+			checkPrimeKernel(candidate,isPrime);
+			return isPrime[0];
+		}
+
+
+		private void checkPrimeKernel(int candidate, boolean[] isPrime) {
+			for(int i=0;i<localPrimes.size();i++) {
+				if(candidate%localPrimes.get(i)==0) {
+					isPrime[0]=false;
+				}
+			}
+			
+		}
+	    
+	    
         }
-    }
+
+
+		
+
+
+    
 }
